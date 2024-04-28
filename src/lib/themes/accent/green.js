244f1e3c2b978180fecdcbaa129ab462f24837e6
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-04-27 23:40:38 -0400

    Fix popups (#45)

--- src/lib/themes/accent/green.js
+++ src/lib/themes/accent/green.js
@@ -1,8 +1,17 @@
 const guiColors = {
+    'motion-primary-transparent': 'hsla(163, 85%, 35%, 0.35)',
+    
     'looks-secondary': 'hsla(163, 85%, 35%, 1)',
     'looks-transparent': 'hsla(163, 85%, 35%, 0.35)',
     'looks-light-transparent': 'hsla(163, 85%, 35%, 0.15)',
-    'looks-secondary-dark': 'hsla(163, 85%, 35%, 1)'
+    'looks-secondary-dark': 'hsla(163, 85%, 35%, 1)',
+
+    'extensions-primary': 'hsla(205, 22%, 65%, 1)',
+    'extensions-tertiary': 'hsla(203, 16%, 22%, 1)',
+    'extensions-transparent': 'hsla(204, 19%, 67%, 0.35)',
+    'extensions-light': 'hsla(203, 16%, 84%, 1)',
+
+    'drop-highlight': '#8ea3b2'
 };
 
 const blockColors = {};
