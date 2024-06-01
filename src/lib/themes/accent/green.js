ce6c4b8b5c3ef82e0ab8ad36e10f0c2379938e47
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-06-01 20:44:15 +0100

    Update green.js

--- src/lib/themes/accent/green.js
+++ src/lib/themes/accent/green.js
@@ -11,9 +11,7 @@ const guiColors = {
     'extensions-transparent': 'hsla(204, 19%, 67%, 0.35)',
     'extensions-light': 'hsla(203, 16%, 84%, 1)',
 
-    'drop-highlight': '#8ea3b2',
-
-    'menu-bar-alternate': 'hsla(215, 60%, 50%, 0)'
+    'drop-highlight': '#8ea3b2'
 };
 
 const blockColors = {};
